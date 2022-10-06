fd4b8f237906a0460d43d32578966bac73d9b3a8
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-10-06 12:10:06 +0300

    Improved Responser

--- App/Views/Responser.php
+++ App/Views/Responser.php
@@ -6,9 +6,9 @@ class Responser
 {
     private array|null $data;
     private array $statuses = [
-        100 => 'Continue',
         200 => 'OK',
         201 => 'Created',
+        204 => 'No Content',
         404 => 'Not Found',
         405 => 'Method Not Allowed',
         500 => 'Internal Server Error',
@@ -17,18 +17,23 @@ class Responser
 
     public function set(array|null $data, string $code)
     {
-        $this->data = $data;
-        $this->code = $code;
+        if(is_null($data)){
+            $this->data = [];
+            $this->code = 204;
+        }
+        if(sizeof($data) === 0) {
+            $this->data = [];
+            $this->code = 204;
+        } else {
+            $this->data = $data;
+            $this->code = $code;
+        }
     }
 
     public function response()
     {
         header("HTTP/1.1 " . $this->code . ' ' . $this->statuses[$this->code]);
         header('Content-Type: application/json; charset=utf-8');
-        if (!is_null($this->data)) {
-            return json_encode($this->data);
-        } else {
-            return null;
-        }
+        return json_encode($this->data);
     }
 }
