d150e91bf073309f87d109de0abc88fbb6e8d17b
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-10-10 12:40:08 +0300

    Refactored, done routes with methods post, patch ,delete

---
+++ App/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Config;
+
+use App\Router\Route;
+
+Route::get('tasks',null, 'index');
+Route::get('auth',null, 'index');
+Route::get('tasks','create', 'create');
+Route::get('tasks','edit', 'edit');
+
+Route::post('tasks', null , 'store');
+Route::patch('tasks', null , 'update');
+Route::delete('tasks', null , 'destroy');
