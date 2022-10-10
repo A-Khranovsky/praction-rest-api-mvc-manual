1aa3f12c794a2606feb8accabda949dd2be39e64
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-10-10 12:58:16 +0300

    Changed Database config

--- App/Config/Database.php
+++ App/Config/Database.php
@@ -12,10 +12,10 @@ class Database
     protected $pdo = null;
     protected const FETCH_ASSOC = PDO::FETCH_ASSOC;
     private const driver = 'mysql';
-    private const host = 'coffee20.mysql.tools';
-    private const userName = 'coffee20_khranovskiy';
-    private const password = '4k3s6vUNkK5H';
-    private const database = 'coffee20_khranovskiy';
+    private const host = '####';
+    private const userName = '####';
+    private const password = '####';
+    private const database = '####';
     private const port = '3306';
     private const charset = 'utf8';
 
