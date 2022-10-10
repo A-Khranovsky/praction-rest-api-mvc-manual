f914d326d98c897116bc88fd4896dd372650ed7c
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-10-10 11:48:26 +0300

    Realized routing using routes

---
+++ App/Config/Routs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Config;
+
+use App\Routes\Route;
+
+//Route::get('tasks',null, 'index');
+//Route::get('tasks','create', 'create');
+//Route::get('tasks','edit', 'edit');
+
+//Route::post();
--- App/Routes/Route.php
+++ App/Routes/Route.php
@@ -2,65 +2,71 @@
 
 namespace App\Routes;
 
-
 use App\Controllers\Controller;
 
 class Route
 {
     private $resource, $router;
+    static private $instance = null;
     static private $routes;
+    static private $GET = [];
+    static private $POST = [];
+    static private $PATCH = [];
+    static private $DELETE = [];
 
-    static public function get($resource, $router)
+    static public function get($resource, $action, $controllerAction)
     {
-        self::$routes = [
-            'tasks' => [
-                '' => 'index',
-                'create' => 'create',
-                'edit' => 'edit'
-            ],
-            'auth' => [
-                '' => 'index'
-            ]
-        ];
-        return new self($resource, $router);
-    }
-    static public function post($resource, $router)
-    {
-        self::$routes = [
-            'tasks' => [
-                ''=>'store'
-            ]
-        ];
-        return new self($resource, $router);
+        self::$GET[] = [$resource, $action, $controllerAction];
+        if (is_null(self::$instance)) {
+            new self;
+        }
     }
 
-    static public function patch($resource, $router)
+    static public function post($resource, $action, $controllerAction)
     {
-        self::$routes = [
-            'tasks' => [
-                '' => 'update'
-            ]
-        ];
-        return new self($resource, $router);
-    }
-//    static public function put($resource, $router)
-//    {
-//    }
-    static public function delete($resource, $router)
-    {
-        self::$routes = [
-            'tasks' => [
-                '' =>'destroy'
-            ]
-        ];
-        return new self($resource, $router);
+        self::$POST[] = [$resource, $action, $controllerAction];
+        if (is_null(self::$instance)) {
+            new self;
+        }
     }
 
-    public function __construct($resource, $router)
+    static public function patch($resource, $action, $controllerAction)
+    {
+        self::$PATCH[] = [$resource, $action, $controllerAction];
+        if (is_null(self::$instance)) {
+            new self;
+        }
+    }
+
+    static public function delete($resource, $action, $controllerAction)
+    {
+        self::$DELETE[] = [$resource, $action, $controllerAction];
+        if (is_null(self::$instance)) {
+            new self;
+        }
+    }
+
+    static public function take()
+    {
+        return self::$instance;
+    }
+
+    public function run($resource, $router)
     {
         $this->router = $router;
         $this->resource = $resource;
-        $controllerAction = self::$routes[$resource][$router->action];
+
+        $routes = self::${$this->router->queryType};
+        foreach ($routes as $route) {
+            if(
+                $route[0] == $resource
+                &&
+                $route[1] == $this->router->action
+            ){
+                $controllerAction = $route[2];
+            }
+        }
+
         $this->router->controllerAction = Controller::run(
             $router->responser,
             $router->resource,
@@ -69,4 +75,52 @@ class Route
             $router->queryParams
         );
     }
+
+    private function __construct()
+    {
+        self::$instance = $this;
+    }
+
+//    static public function get($resource, $router)
+//    {
+//        self::$routes = [
+//            'tasks' => [
+//                '' => 'index',
+//                'create' => 'create',
+//                'edit' => 'edit'
+//            ],
+//            'auth' => [
+//                '' => 'index'
+//            ]
+//        ];
+//        return new self($resource, $router);
+//    }
+//    static public function post($resource, $router)
+//    {
+//        self::$routes = [
+//            'tasks' => [
+//                ''=>'store'
+//            ]
+//        ];
+//        return new self($resource, $router);
+//    }
+//
+//    static public function patch($resource, $router)
+//    {
+//        self::$routes = [
+//            'tasks' => [
+//                '' => 'update'
+//            ]
+//        ];
+//        return new self($resource, $router);
+//    }
+//    static public function delete($resource, $router)
+//    {
+//        self::$routes = [
+//            'tasks' => [
+//                '' =>'destroy'
+//            ]
+//        ];
+//        return new self($resource, $router);
+//    }
 }
